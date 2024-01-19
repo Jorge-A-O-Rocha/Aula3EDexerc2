d69064605f4aa5887bd9471084d54afe4b399860
Jorge  Augusto de Oliveira Rocha <84191109+Jorge-A-O-Rocha@users.noreply.github.com>
2024-01-19 16:08:14 -0300

    Update main.java

--- src/view/main.java
+++ src/view/main.java
@@ -9,9 +9,9 @@ public class main {
 	public static void main(String[] args) {
 	modulos mod = new modulos();
 		
-		int x = Integer.parseInt(JOptionPane.showInputDialog("Somar até "));
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Número a ser convertido "));
 		String bin = mod.binario(x);
-		System.out.println("Somatória = " + bin);
+		System.out.println("Binário : " + bin);
 	}
 
 }
